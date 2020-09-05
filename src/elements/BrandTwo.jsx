4d94f243ae2184d3058480a612b03b6dc449f04b
Marcella Gutierrez <marcella@MacBook-Pro-de-Marcella.local>
2020-09-05 20:37:25 -0300

    update brands

--- src/elements/BrandTwo.jsx
+++ src/elements/BrandTwo.jsx
@@ -6,31 +6,13 @@ class BrandTwo extends Component{
             <React.Fragment>
                 <ul className="brand-style-2">
                     <li>
-                        <img src="/assets/images/brand/brand-01.png" alt="Logo Images"/>
+                        <img src="/assets/images/brand/1.png" alt="sororidade"/>
                     </li>
                     <li>
-                        <img src="/assets/images/brand/brand-02.png" alt="Logo Images"/>
+                        <img src="/assets/images/brand/2.png" alt="Equilíbrio"/>
                     </li>
                     <li>
-                        <img src="/assets/images/brand/brand-03.png" alt="Logo Images"/>
-                    </li>
-                    <li>
-                        <img src="/assets/images/brand/brand-04.png" alt="Logo Images"/>
-                    </li>
-                    <li>
-                        <img src="/assets/images/brand/brand-05.png" alt="Logo Images"/>
-                    </li>
-                    <li>
-                        <img src="/assets/images/brand/brand-06.png" alt="Logo Images"/>
-                    </li>
-                    <li>
-                        <img src="/assets/images/brand/brand-02.png" alt="Logo Images"/>
-                    </li>
-                    <li>
-                        <img src="/assets/images/brand/brand-03.png" alt="Logo Images"/>
-                    </li>
-                    <li>
-                        <img src="/assets/images/brand/brand-04.png" alt="Logo Images"/>
+                        <img src="/assets/images/brand/3.png" alt="Código"/>
                     </li>
                 </ul>
             </React.Fragment>
